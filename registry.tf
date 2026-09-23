c348a1d2412b55cf7e9a15afe8d217c9ee062bff
Astro Pryor <182395467+AstroPryor@users.noreply.github.com>
2026-09-23 14:13:18 -0500

    Update registry.tf

--- registry.tf
+++ registry.tf
@@ -1,12 +1,12 @@
 resource "aws_ecr_repository" "app" {
-    name                 = "astro-app"
-    image_tag_mutability = "IMMUTABLE"
+  name                 = "astro-app"
+  image_tag_mutability = "IMMUTABLE"
 
-    image_scanning_configuration {
-        scan_on_push = true
-    }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-    tags = {
-        Name = "Astro - ECR Repo"
-    }
+  tags = {
+    Name = "Astro - ECR Repo"
+  }
 }
