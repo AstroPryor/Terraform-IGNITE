5c1fb9aaa379e3c25526f324b5c9d70656659a35
Astro Pryor <182395467+AstroPryor@users.noreply.github.com>
2026-09-23 14:13:10 -0500

    Update alarms.tf

--- alarms.tf
+++ alarms.tf
@@ -1,18 +1,18 @@
 resource "aws_cloudwatch_metric_alarm" "app_cpu_high" {
-    alarm_name          = "astro-app-cpu-high"
-    comparison_operator = "GreaterThanThreshold"
-    evaluation_periods   = 2
-    metric_name         = "CPUUtilization"
-    namespace           = "AWS/ECS"
-    period              = 300
-    statistic           = "Average"
-    threshold           = 80
+  alarm_name          = "astro-app-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
 
-    dimensions = {
-        ClusterName = aws_ecs_cluster.main.name
-        ServiceName = aws_ecs_service.app.name
-    }
+  dimensions = {
+    ClusterName = aws_ecs_cluster.main.name
+    ServiceName = aws_ecs_service.app.name
+  }
 
-    alarm_description = "Triggers when astro-app-service average CPU utilization exceeds 80% for 10 minutes"
-    treat_missing_data = "notBreaching"
+  alarm_description  = "Triggers when astro-app-service average CPU utilization exceeds 80% for 10 minutes"
+  treat_missing_data = "notBreaching"
 }
