8b09a435bf032003e60d3a47583af2caf9a25705
Astro Pryor <182395467+AstroPryor@users.noreply.github.com>
2026-09-23 14:11:22 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "astro-terraform-state-685306736016"
+    key            = "astro-app/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "astro-tf-lock"
+    encrypt        = true
+  }
 }
 
 # Configure the AWS Provider
