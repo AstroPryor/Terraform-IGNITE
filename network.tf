17a8ea7949e76853e31767e9cfd8de926524cad4
Astro Pryor <182395467+AstroPryor@users.noreply.github.com>
2026-09-23 14:11:18 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -60,11 +60,11 @@ resource "aws_security_group" "web" {
     vpc_id = aws_vpc.example.id
 
     ingress {
-        description = "HTTP"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        description     = "HTTP from ALB"
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        security_groups = [aws_security_group.alb.id]
     }
 
     egress {
